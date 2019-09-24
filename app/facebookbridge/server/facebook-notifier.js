cafd865ff66b127c4a5a604ee1b61678dcafda1e
adamkac <adaskaczmarzyk@gmail.com>
2019-09-24 22:08:46 +0200

    * facebook bridge

---
+++ app/facebookbridge/server/facebook-notifier.js
@@ -0,0 +1,24 @@
+import FacebookBridge from "./facebook-bridge";
+import { callbacks } from "../../callbacks";
+
+callbacks.add(
+  "afterSaveMessage",
+  function(message, room) {
+    if (room && room.t === "l" && room.v._id !== message.u._id) {
+      console.log("notify", message);
+
+      var user = Meteor.users.findOne({
+        _id: room.v._id,
+        type: "visiter",
+        visiter_type: "facebook",
+      });
+      if (user) {
+        FacebookBridge.sendMessage(user, message);
+      }
+    }
+
+    return message;
+  },
+  callbacks.priority.LOW,
+  "notifyFacebookUserOnMessage"
+);
---
+++ app/facebookbridge/server/index.js
@@ -0,0 +1,7 @@
+import FacebookBridge from './facebook-bridge';
+import './settings';
+new FacebookBridge();
+
+export {
+	FacebookBridge,
+};
